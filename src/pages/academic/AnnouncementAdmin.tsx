c72615ed3c01782dbdd77ac59092fc1f9224d390
alfina zahra <alfinaz916@gmail.com>
2025-05-30 21:06:00 +0700

    feat: add academic-page

---
+++ src/pages/academic/AnnouncementAdmin.tsx
@@ -0,0 +1,9 @@
+const AnnouncementAdmin = () => {
+  return (
+    <div className="text-black text-xl font-semibold mt-20 mx-11">
+      Pengumuman
+    </div>
+  );
+};
+
+export default AnnouncementAdmin;
--- src/routes/router/private.tsx
+++ src/routes/router/private.tsx
@@ -1,8 +1,35 @@
 import { RouteObject } from "react-router-dom";
+import DashboardAcademic from "../../pages/academic/DashboardAcademic";
+import SidebarAcademicLayout from "../../components/layout/SidebarAcademicLayout";
+import AnnouncementAdmin from "../../pages/academic/AnnouncementAdmin";
+import PartnerAdmin from "../../pages/academic/PartnerAdmin";
+import ProfileAcademic from "../../pages/academic/profile/ProfileAcademic";
 
 export const privateRoute: RouteObject[] = [
   {
     path: "/home",
     element: <div>home page</div>,
+  },
+  {
+    path: "",
+    Component: SidebarAcademicLayout,
+    children: [
+      {
+        path: "/dashboard/academic",
+        element: <DashboardAcademic />,
+      },
+      {
+        path: "/pengumuman",
+        element: <AnnouncementAdmin />,
+      },
+      {
+        path: "/mitra-pkl",
+        element: <PartnerAdmin />,
+      },
+      {
+        path: "/profile",
+        element: <ProfileAcademic />,
+      },
+    ],
   },
 ];
