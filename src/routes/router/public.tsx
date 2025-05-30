db7663b86fd800740b28eb546c1a9ca30db3919a
alfina zahra <alfinaz916@gmail.com>
2025-05-30 07:30:22 +0700

    feat: add hook and service register admin

--- src/routes/router/public.tsx
+++ src/routes/router/public.tsx
@@ -2,6 +2,8 @@ import { RouteObject } from "react-router-dom";
 import RegisterStudent from "../../pages/register/RegisterStudent";
 import AuthLayout from "../../components/layout/AuthLayout";
 import Login from "../../pages/Login";
+import RegisterAcademic from "../../pages/register/RegisterAcademic";
+import RegisterLecturer from "../../pages/register/RegisterLecturer";
 
 export const publicRoute: RouteObject[] = [
   {
@@ -11,6 +13,14 @@ export const publicRoute: RouteObject[] = [
       {
         path: "sign-up",
         element: <RegisterStudent />,
+      },
+            {
+        path: "sign-up-academic",
+        element: <RegisterAcademic />,
+      },
+      {
+        path: "sign-up-lecturer",
+        element: <RegisterLecturer />,
       },
       {
         path: "sign-in",
